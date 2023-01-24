8ba74684195546b8bab551b9193b82e84501f12c
automatether <83187517+automatether@users.noreply.github.com>
2023-01-24 06:21:49 -0800

    Add files via upload

---
+++ H1_src/control_design.m
@@ -0,0 +1,63 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Hybrid and Embedded control systems
+% Homework 1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Initialization
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear
+init_tanks;
+g = 9.82;
+Tau = 1/alpha1*sqrt(2*tank_h10/g);
+k_tank = 60*beta*Tau;
+gamma_tank = alpha1^2/alpha2^2;
+uss = alpha2/beta*sqrt(2*g*tank_init_h2)*100/15; % steady state input
+yss = 40; % steady state output
+
+%added parameters
+chi=0.5;
+zeta=0.7;
+omega0=0.1;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Continuous Control design
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+uppertank = tf(k_tank,[1 Tau]); % Transfer function for upper tank
+lowertank = tf(gamma_tank,[1 Tau*gamma_tank]); % Transfer function for upper tank
+G = uppertank*lowertank; % Transfer function from input to lower tank level
+
+% Calculate PID parameters
+[K, Ti, Td, N]=polePlacePID(chi, omega0, zeta, Tau, gamma_tank, k_tank);
+s=tf('s');
+F=K*(1+(1/(Ti*s)+(Td*N*s/(s+N))))
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Digital Control design
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Ts = 1; % Sampling time
+
+% Discretize the continous controller, save it in state space form
+% [A_discretized,B_discretized,C_discretized,D_discretized] =
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Discrete Control design
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Discretize the continous state space system, save it in state space form
+% [Phi,Gamma,C,D] = 
+
+% Observability and reachability
+Wc = 1;
+Wo = 1;
+
+% State feedback controller gain
+L = 1;
+% observer gain
+K = 1;
+% reference gain
+lr = 1;
+
+% augmented system matrices
+Aa = 1;
+Ba = 1;
